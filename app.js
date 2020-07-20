8e8d4027078812269c21dfa7c011a58caddf9d69
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2020-07-20 12:03:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContact = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContact = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContact = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [{
+    title: 'About Our Blogs',
+    content: 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.'
+}];
+console.log(posts);
+
+app.get("/", function(req, res) {
+    res.render("home", { homeSC: homeStartingContact, posts: posts });
+});
+
+app.get("/about", function(req, res) {
+    res.render("about", { abContact: aboutContact });
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact", { cC: contactContact });
+});
+
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
+
+app.post("/", function(req, res) {
+    const post = {
+        title: req.body.blogTitle,
+        content: req.body.blogBody
+    };
+
+    posts.push(post);
+
+    res.redirect("/");
+});
+
+
+app.get("/posts/:postName", function(req, res) {
+    const typed = _.lowerCase(req.params.postName);
+
+    posts.forEach(function(post) {
+        const stored = _.lowerCase(post.title);
+
+        if (stored === typed) {
+            console.log("Match Found!!!");
+            res.render("post", {
+                title: post.title,
+                content: post.content
+            });
+        }
+    });
+});
+
+
+app.listen(3000, function() {
+    console.log("server is up and running on port 3000");
+});
