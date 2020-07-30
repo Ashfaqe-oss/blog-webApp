23ea9733dc0da63644a58ad580fa16993ae78463
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2020-07-30 12:13:27 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContact = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContact = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,15 +17,33 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [{
-    title: 'About Our Blogs',
-    content: 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.'
-}];
-console.log(posts);
+mongoose.connect("mongodb+srv://Admin-Ashfaqe:v0noXyDfSuPoVymq@cluster0.4cxwc.mongodb.net/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = {
+    title: String,
+    content: String
+};
+
+const Post = new mongoose.model("Post", postSchema);
+
+
+const post1 = new Post({
+    title: "About Our Blogs",
+    content: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+});
+
 
 app.get("/", function(req, res) {
-    res.render("home", { homeSC: homeStartingContact, posts: posts });
+
+    Post.find({}, function(err, posts) {
+        res.render("home", {
+            homeSC: homeStartingContact,
+            posts: posts
+        });
+
+    });
 });
+
 
 app.get("/about", function(req, res) {
     res.render("about", { abContact: aboutContact });
@@ -39,37 +58,46 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    const post = {
+    const post = new Post({
         title: req.body.blogTitle,
         content: req.body.blogBody
-    };
+    });
 
-    posts.push(post);
+    post.save(function(err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
 
-    res.redirect("/");
 });
 
 
-app.get("/posts/:postName", function(req, res) {
-    const typed = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function(req, res) {
+    //const typed = _.lowerCase(req.params.postName);
+    const requestedPostId = req.params.postId;
 
-    posts.forEach(function(post) {
-        const stored = _.lowerCase(post.title);
-
-        if (stored === typed) {
-            console.log("Match Found!!!");
-            res.render("post", {
-                title: post.title,
-                content: post.content
-            });
-        }
+    Post.findOne({ _id: requestedPostId }, function(err, post) {
+        res.render("post", {
+            title: post.title,
+            content: post.content
+        });
     });
+
+    //Post.forEach(function(post) {
+    //const stored = _.lowerCase(post.title);
+
+    //if (stored === typed) {
+    // console.log("Match Found!!!");
+
+    //}
+    //});
 });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
 }
+
 app.listen(port, function() {
     console.log("server is up and running on port 3000");
 });
